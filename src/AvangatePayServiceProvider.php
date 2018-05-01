f13802982d2b1d3bd98ad165b7fd40d87c00b836
root <root@sd-56562.dedibox.fr>
2018-05-01 15:07:02 +0200

    First

---
+++ src/AvangatePayServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+namespace Avangate\AvangatePayLaravel;
+
+use Illuminate\Support\ServiceProvider;
+class AvangatePayServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+      
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/config.php',
+            'iyzipay'
+        );
+      
+        $this->app->bind('avangate-laravel', function () {
+            return new AvangatePayLaravel();
+        });
+    }
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['avangate-laravel'];
+    }
+}
